a02963aaeeb36926a2d3023eebb6b99a02c7ba6a
VaishnaviYamsani <yamsanivaishnavi03@gmail.com>
2023-03-11 06:38:44 +0530

    addition

---
+++ ne.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.*;
+
+public class ne{
+	public static void main(String[] args){
+		System.out.println("Hello");
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=a+b;
+		System.out.println(c);
+	}
+}
